1802d224a5bf5fa81f63c9d56c69e8ba502b3da8
Jake <jakedves@gmail.com>
2023-04-16 14:47:18 +0100

    Initial Commit

---
+++ Ultimate Cat Viewer/Ultimate_Cat_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ultimate_Cat_ViewerApp.swift
+//  Ultimate Cat Viewer
+//
+//  Created by Jake Davies on 16/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Ultimate_Cat_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
